82073d2ebce6e624271b9cd6ddbb3dd1c1509c7f
georgeyv <8842869+georgeyv@user.noreply.gitee.com>
2022-10-14 17:55:04 +0800

    z

--- app/Controller/LOLController.php
+++ app/Controller/LOLController.php
@@ -11,18 +11,32 @@ declare(strict_types=1);
  */
 namespace App\Controller;
 
+use App\Constants\HttpCode;
+use App\Exception\BusinessException;
 use App\Model\Games;
 use App\Model\LoL\Heros;
 use App\Request\HeroListRequest;
+use App\Request\LOL\HeroInfoRequest;
 use App\Utils\ApiResponseTrait;
 
 class LOLController extends AbstractController
 {
     use ApiResponseTrait;
 
-    public function homeList(HeroListRequest $request)
+    public function heroInfo(HeroInfoRequest $request)
     {
+        var_dump($request->all());
 
+        if (!$request->has(['id','page','limit'])){
+            throw new BusinessException(HttpCode::LogicError,'参数不完整');
+        }
+        $request->validated();
+        $id = $request->input('id');
+        $skinId = $request->input('skinId');
+        $page = $request->input('page');
+        $limit = $request->input('limit');
+        $data = Heros::getHeroInfo($id,$skinId,$page,$limit);
+        return $this->responseSuccess('获取成功',$data);
     }
 
 }
